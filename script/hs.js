1ca6e9231eec628fb9dc31e445fb0c4aa0b9bbd4
billysingo <weekendy963@gmail.com>
2022-11-18 08:45:18 +0800

    Update hs.js

--- script/hs.js
+++ script/hs.js
@@ -1,3 +1,4 @@
 let body = JSON.parse($response.body);
-body['res']['hs']['collectTime']=new Date(Date.now()-86400000).toLocaleString()
+body['res']['hs']['collectTime']=new Date(Date.now()-86400000).toLocaleString();
+console.log('change collectTime to '+body['res']['hs']['collectTime']+' done!');
 $done({body: JSON.stringify(body)});
