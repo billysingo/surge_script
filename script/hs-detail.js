5fdfcf674766e0d71768ef91e5aee5a31d5f145f
billysingo <weekendy963@gmail.com>
2022-12-08 08:17:27 +0800

    update

--- script/hs-detail.js
+++ script/hs-detail.js
@@ -1,5 +1,4 @@
 let body = JSON.parse($response.body);
-body['reportList'][1]['checkTime']=new Date(Date.now()-81400000).toLocaleString();
-body['reportList'][1]['collectTime']=new Date(Date.now()-86400000).toLocaleString();
-console.log('change hs detail  done!');
+body['res']['collectTime']=new Date(Date.now()-86400000).toLocaleString();
+console.log('change hs detail done!');
 $done({body: JSON.stringify(body)});
