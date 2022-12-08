9411b93011a438b1cd134c4920c8c45cc1bee3e7
billysingo <weekendy963@gmail.com>
2022-12-08 08:55:03 +0800

    update time to 23:00:00 +rand

--- script/hs.js
+++ script/hs.js
@@ -1,4 +1,4 @@
 let body = JSON.parse($response.body);
-body['res']['hs']['collectTime']=new Date(Date.now()-86400000).toLocaleString().replaceAll("/","-");
+body['res']['hs']['collectTime']=new Date(Date.now()-86400000).toLocaleDateString().replaceAll("/","-")+" 23:"+parseInt(Math.random()*49+10)+":23";
 console.log('change collectTime to '+body['res']['hs']['collectTime']+' done!');
 $done({body: JSON.stringify(body)});
