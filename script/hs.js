da804af4ad1e99c3f0b27149cf8f3b718b75289b
billysingo <weekendy963@gmail.com>
2022-12-08 08:30:54 +0800

    hs

--- script/hs.js
+++ script/hs.js
@@ -1,4 +1,4 @@
 let body = JSON.parse($response.body);
-body['res']['hs']['collectTime']=new Date(Date.now()-86400000).toLocaleString();
+body['res']['hs']['collectTime']=new Date(Date.now()-86400000).toLocaleString().replaceAll("/","-");
 console.log('change collectTime to '+body['res']['hs']['collectTime']+' done!');
 $done({body: JSON.stringify(body)});
